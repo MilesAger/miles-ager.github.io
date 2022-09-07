3e2ca46189e096ced3fcc8856299cde8d9b4c6f4
Miles Ager <miles.ager13@gmail.com>
2022-09-07 14:00:08 +0000

    update studies

--- studies/control-flow.js
+++ studies/control-flow.js
@@ -1,6 +1,35 @@
 // Control-flow +++++++++++++++++++++++++++
-// when a computer runs code, it always runs code from top to bottom. this concept is known as Control-Flow. Example seen below.
-var change = 'something';
-change = 'something else';
-console.log(change); //expect this to equal 'something else'
-// Due to the control-flow designating the code to run top to bottom, the variable change is assigned the value of 'something' on line 3, and, due to the control-flow, the variable change does not hold any influence earlier in the code than line 3. If the string value of change is returned after the reassignment seen on line 4, it will return the value 'something else'. This further emphasizes the timeline effect resulting from the control-flow.   
+// 0. conditional statements allow us to control the flow of our applicating. based on whether conditions are true or false, we are able to control whether a block of code runs.
+//    conditional statements are comparisons and/or values evaluted to either true or false.
+// 1. if, else-if, and else statements. These are the 3 keywords typically used in conditional statems. 
+// conditional statements will typically start with an if statement, followed by an else if statement to test other conditionals, followed by the else statement which has no conditional. an example of an if, else-if, else statement can be seen in the below example.
+var a = 10;
+
+if (a <= 5){
+    console.log('small');
+}
+else if (a > 5 && a <= 10){
+    console.log('medium');
+} else {
+    console.log('large');
+}; 
+//after this code is run 'medium' will be logged to the console. because the condition in the else if coindition is met. 
+//The if, else-if, else statement can chain together any number of else-if statements to the first if. each conditional statement tests the condition in parenthesis () and then executes the code in the body if the condition is true.
+// 2. Switch Statements.
+// switch Statements can sometimes be a cleaner way to test a large amount of conditions.
+// switch statement evaluates an input expression. the expression value is then tested whether any of the case clauses match the input expression. if a case is matched, then the following statement associated with the case is executed. the break statement is used to end the case. example seen below.
+var animal = 'dog';
+switch (animal){
+    case 'cat' :
+        console.log('meow');
+        break;
+        case 'parrot' :
+            console.log('chirp');
+            break;
+            case 'dog' :
+                console.log('woof');
+                break;
+                default: 
+                console.log('none found')
+}
+//the above code will result in 'woof' being logged to the console.
